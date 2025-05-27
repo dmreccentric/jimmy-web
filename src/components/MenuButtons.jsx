df9b2f37c0e0e1de225029cce3318bf008f13bf3
dmreccentric <eccentricecc481@gmail.com>
2025-05-27 18:54:35 +0100

    fixed menu buttons scroll effect

--- src/components/MenuButtons.jsx
+++ src/components/MenuButtons.jsx
@@ -14,19 +14,23 @@ function MenuButtons({ categories, filterItems }) {
       <h2 className="text-center font-bold text-[34px] mb-[2rem] p-3 border-black border-b-[3px] w-fit text-blue">
         My Menu
       </h2>
-      <div className="flex gap-5 justify-center mb-[5rem]">
-        {categories.map((category, index) => (
-          <button
-            type="button"
-            key={index}
-            onClick={() => handleSelect(category, index)}
-            className={`${
-              indexValue === index ? "bg-blue" : ""
-            } border-2 p-2  border-blue hover:bg-blue hover:text-white capitalize text-base rounded-lg cursor-pointer bg-transparent tracking-[1px] font-medium hover:borer-b-black`}
-          >
-            {category}
-          </button>
-        ))}
+
+      {/* Scrollable container */}
+      <div className="w-full overflow-x-auto hide-scrollbar flex sm:justify-center mb-11">
+        <div className="flex gap-5 px-4 w-max min-w-fit">
+          {categories.map((category, index) => (
+            <button
+              type="button"
+              key={index}
+              onClick={() => handleSelect(category, index)}
+              className={`${
+                indexValue === index ? "bg-blue text-white" : ""
+              } border-2 p-2 border-blue hover:bg-blue hover:text-white capitalize text-base rounded-lg cursor-pointer tracking-[1px] font-medium whitespace-nowrap`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );

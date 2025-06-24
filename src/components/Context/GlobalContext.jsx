b5a712c3ff2f43442aa2984dd9e8010233bf13ca
dmreccentric <eccentricecc481@gmail.com>
2025-06-24 13:45:00 +0100

    added local storage

--- src/components/Context/GlobalContext.jsx
+++ src/components/Context/GlobalContext.jsx
@@ -1,20 +1,25 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 export const GlobalContext = createContext(null);
 const GlobalContextProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const saveData = localStorage.getItem("cartItems");
+    return saveData ? JSON.parse(saveData) : [];
+  });
   const [user, setUser] = useState(null);
   const [deleteCartItems, setDeleteCartItems] = useState(false);
   const [count, setCount] = useState(1);
   const [cart, setCart] = useState(null);
-  const deleteCartItem = (id) => {
-    // console.log(id, "cart id");
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
-    // console.log("delete cart");
+  const deleteCartItem = (id) => {
     const checkCartItem = cartItems.filter((item) => item.id !== id);
     console.log(checkCartItem, "new array");
 
     setCartItems(checkCartItem);
   };
+
   const GlobalValue = {
     cartItems,
     setCartItems,

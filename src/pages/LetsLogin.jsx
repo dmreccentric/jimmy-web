a31b5604ad490ab602adcacb86391b383fa3e7b3
dmreccentric <eccentricecc481@gmail.com>
2025-08-03 16:11:01 +0100

    fixed registration issue

--- src/pages/LetsLogin.jsx
+++ src/pages/LetsLogin.jsx
@@ -16,7 +16,7 @@ const LetsLogin = () => {
 
   return (
     <section className="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center px-4">
-      <div className="w-full max-w-md bg-white dark:bg-gray-800 shadow-md rounded-lg">
+      <div className="w-full max-w-md bg-white dark:bg-gray-800 shadow-md rounded-lg p-8">
         <p
           ref={errRef}
           className={`${
